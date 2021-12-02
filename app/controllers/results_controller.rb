863c5bd2a2a1b76cb7818c1c3bb4d7024f3db589
lucasduff-code <lucasjduff@gmail.com>
2021-12-02 10:45:30 -0600

    changed reference to flash poll_id

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,7 +5,7 @@ class ResultsController < ApplicationController
 
     poll_identifier = params[:poll_identifier]
 
-    @poll = Poll.where("poll_identifier = '#{poll_identifier}'").first
+    @poll = Poll.where("poll_identifier = '#{flash[:poll_id]}'").first
     @timeslots = Timeslot.where("poll_id = '#{@poll.id}'")
     @title = @poll.poll_name
     @reservers = Reserver.where("poll_id = '#{@poll.id}'")
