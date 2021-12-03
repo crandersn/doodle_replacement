6f5a5f0ea00eee8e87e94d6d5886e252bb7522c4
lucasduff-code <lucasjduff@gmail.com>
2021-12-02 22:20:04 -0600

    view results page is now in working order

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -13,16 +13,66 @@ class ResultsController < ApplicationController
     @hash_arr = []
     @timeslots.each do |timeslot|
       puts timeslot
+
       @reservers = Reserver.where("timeslot_id = '#{timeslot.id}'")
+
+      slot = {}
+      #slot[:start] = timeslot.start_time
+
+      raw_datetime_start = timeslot.start_time.delete(",").split(" ")
+      
+      year_start = raw_datetime_start[3].to_i
+      month_start = raw_datetime_start[2].downcase
+      day_month_start = raw_datetime_start[1].to_i
+
+      atomic_start = raw_datetime_start[4].split(":")
+      hours_start = atomic_start[0].to_i
+      minutes_start = atomic_start[1].to_i
+      seconds_start = 0
+
+      t_start = Time.utc(year_start,month_start,day_month_start,hours_start,minutes_start,seconds_start)
+      slot[:start] = t_start.localtime.strftime("%Y-%m-%d %l:%M:%S %p")
+
+      raw_datetime_end = timeslot.end_time.delete(",").split(" ")
+
+      year_end = raw_datetime_end[3].to_i
+      month_end = raw_datetime_end[2].downcase
+      day_month_end = raw_datetime_end[1].to_i
+
+      atomic_end = raw_datetime_end[4].split(":")
+      hours_end = atomic_end[0].to_i
+      minutes_end = atomic_end[1].to_i
+      seconds_end = 0
+      t_end = Time.utc(year_end,month_end,day_month_end,hours_end,minutes_end,seconds_end)
+      slot[:end] = t_end.localtime.strftime("%Y-%m-%d %l:%M:%S %p")
+      #slot[:end] = timeslot.end_time
+      slot[:available] = timeslot.available
+      slot[:num_votes] = timeslot.num_votes
+
       reserverHash = {timeslot.id => []}
-      reserver_arr = []
+      reserver_string = ""
       @reservers.each do |reserver|
-        reserver_arr.push(reserver.name)
+        reserver_string += reserver.name + ", "
+        #reserver_arr.push(reserver.name)
       end
-      reserverHash[timeslot.id] = reserver_arr
-      @hash_arr.push(reserverHash)
+      reserver_string = reserver_string[0...-2]
+      if reserver_string == ""
+        reserver_string = "No Reservers"
+      end
+      slot[:reservers] = reserver_string
+      #reserverHash[timeslot.id] = reserver_arr
+      #@hash_arr.push(reserverHash)
+      #
+      @hash_arr.push(slot)
     end
 
+    # @hash_arr_dates = []
+    # datesHash = {}
+    # @timeslots.each do |timeslot|
+    #   datesHash[timeslot.id] = timeslot.date
+    #   @hash_arr_dates.push(datesHash)
+    # end
+    #
     puts(@hash_arr)
 
   end
